4a215ca66024203b4ad4a9ae32a32dfde46816ac
zaida <zaida.araca89@gmail.com>
2023-07-05 00:51:12 -0400

    feature(common): creation api rest for products[WEBSITES-2]

---
+++ src/routes/products.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { get, getAll, post, put, deleted } from '../websites-2/controller/products.controller';
+
+const router = Router()
+
+router.get('/',get)
+router.get('/:id',getAll)
+router.post('/',post)
+router.put('/:id',put)
+router.delete('/:id',deleted)
+
+export{router}
